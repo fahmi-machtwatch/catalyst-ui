17e8b6183bf16f691b3306c2b9f8b0bab8e336eb
Fahmi Idris <fahmi@Fahmi-Idris.local>
2022-10-26 15:59:50 +0700

    feat: change react forwardref to chakra

--- packages/xms/src/components/pagination/components/PaginationButton.tsx
+++ packages/xms/src/components/pagination/components/PaginationButton.tsx
@@ -1,34 +1,32 @@
-import { Button } from '@chakra-ui/react';
 import React from 'react';
+import { Button, ButtonProps, forwardRef } from '@chakra-ui/react';
 
-export interface PaginationButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+export interface PaginationButtonProps {
   isActive?: boolean;
 }
 
-const PaginationButton = React.forwardRef<HTMLButtonElement, PaginationButtonProps>(
-  ({ className, style, isActive, children, ...rest }, ref) => {
-    return (
-      <Button
-        ref={ref}
-        className={className}
-        borderRadius={50}
-        fontWeight={700}
-        m={0}
-        p={3}
-        h={30}
-        style={style}
-        minW="auto"
-        background={isActive ? 'primary.500' : 'secondary.50'}
-        color={isActive ? 'white' : 'black'}
-        _hover={!isActive ? { background: 'secondary.50', color: 'primary.500' } : {}}
-        _disabled={{ background: 'secondary.50', pointerEvents: 'none' }}
-        {...rest}
-      >
-        {children}
-      </Button>
-    );
-  }
-);
+const PaginationButton = forwardRef<ButtonProps, 'button'>(({ className, style, isActive, children, ...rest }, ref) => {
+  return (
+    <Button
+      ref={ref}
+      className={className}
+      borderRadius={50}
+      fontWeight={700}
+      m={0}
+      p={3}
+      h={30}
+      style={style}
+      minW="auto"
+      background={isActive ? 'primary.500' : 'secondary.50'}
+      color={isActive ? 'white' : 'black'}
+      _hover={!isActive ? { background: 'secondary.50', color: 'primary.500' } : {}}
+      _disabled={{ background: 'secondary.50', pointerEvents: 'none' }}
+      {...rest}
+    >
+      {children}
+    </Button>
+  );
+});
 
 PaginationButton.displayName = 'PaginationButton';
 
--- packages/xms/src/components/pagination/components/PaginationButtonTrigger.tsx
+++ packages/xms/src/components/pagination/components/PaginationButtonTrigger.tsx
@@ -4,11 +4,11 @@ import { VisuallyHidden } from '@chakra-ui/react';
 import PaginationButton from './PaginationButton';
 
 type PaginationButtonTriggerProps = {
-  color: string;
-  disabled: boolean;
-  onClick: () => void;
-  visuallyHidden: string;
-  icon: React.ReactNode;
+  color?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+  visuallyHidden?: string;
+  icon?: React.ReactNode;
 };
 
 const PaginationButtonTrigger: React.FC<PaginationButtonTriggerProps> = ({
@@ -17,13 +17,19 @@ const PaginationButtonTrigger: React.FC<PaginationButtonTriggerProps> = ({
   onClick,
   visuallyHidden,
   icon,
-}) => {
-  return (
-    <PaginationButton color={color} disabled={disabled} onClick={onClick}>
-      <VisuallyHidden>{visuallyHidden}</VisuallyHidden>
-      {icon}
-    </PaginationButton>
-  );
+}) => (
+  <PaginationButton color={color} disabled={disabled} onClick={onClick}>
+    <VisuallyHidden>{visuallyHidden}</VisuallyHidden>
+    {icon}
+  </PaginationButton>
+);
+
+PaginationButtonTrigger.defaultProps = {
+  color: undefined,
+  disabled: undefined,
+  onClick: undefined,
+  visuallyHidden: undefined,
+  icon: undefined,
 };
 
 export default PaginationButtonTrigger;
